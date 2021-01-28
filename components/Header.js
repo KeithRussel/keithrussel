d1b9cf2456afabcf8dfc22ec2acfbe2e0461ab10
KeithRussel <keithmaniacop@gmail.com>
2021-01-29 00:27:44 +0800

    Portfolio using Nextjs WIP update

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.wrapper}>
+        <h2>My Logo</h2>
+        <ul className={styles.headerNav}>
+          <li>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/about'>
+              <a>About Us</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,11 @@ import Head from 'next/head';
 import styles from '../styles/Layout.module.css';
 
 function HomePage() {
-  return <h1>Welcome to Next.js!</h1>;
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+    </div>
+  );
 }
 
 export default HomePage;
