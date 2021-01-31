1ba4067ce19cc156d4bf94722c3b3ef0060eb768
KeithRussel <keithmaniacop@gmail.com>
2021-02-01 00:51:25 +0800

    Add fontawesome and unfinished mobile nav

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/Header.module.css';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
   return (
     <div className={styles.header}>
+      <div className={styles.mobileHeaderNav}>
+        <ul>
+          <li>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/about'>
+              <a>About Us</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
       <div className={styles.wrapper}>
         <h2>My Logo</h2>
         <ul className={styles.headerNav}>
@@ -19,6 +35,9 @@ const Header = () => {
             </Link>
           </li>
         </ul>
+        <div className={styles.mobileNav}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
       </div>
     </div>
   );
