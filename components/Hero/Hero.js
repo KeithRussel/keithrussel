30bff9aba1a46971fd5265b54f5a3d7430489cfe
KeithRussel <keithmaniacop@gmail.com>
2022-11-03 15:20:42 +0800

    Remove unused import

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import styles from "../../styles/landingpage/Hero.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCodepen,
   faLinkedin,
