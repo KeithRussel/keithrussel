c742517479df2b31d8570933513e5c406262c201
KeithRussel <keithmaniacop@gmail.com>
2022-11-03 15:19:42 +0800

    Modify Landing Page to make a cleaner component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Skills from "../components/Skills";
-import Hero from "../components/Hero";
+import Hero from "../components/Hero/Hero";
 
 function HomePage() {
   return (
@@ -10,7 +10,13 @@ function HomePage() {
         <title>Keith Russel | Home</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Hero />
+      <Hero
+        mypic={"/uploads/image1.jpg"}
+        name={"Russel Maniacop"}
+        description={
+          "I'm a code hobbyist specializing in Web Development. Bringing designs into live website and webapp projects."
+        }
+      />
       <Skills />
     </>
   );
